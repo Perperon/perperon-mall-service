66acad0f1b3f02b9423cea33ebd873227eec3c48
Perperon <du1443528055@163.com>
2024-04-19 11:30:17 +0800

    pom依赖调整

--- perperon-test/src/main/java/com/perperon/mall/config/GatewayConfig.java
+++ perperon-test/src/main/java/com/perperon/mall/config/GatewayConfig.java
@@ -1,4 +1,3 @@
-/*
 package com.perperon.mall.config;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -6,12 +5,11 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-*/
 /**
  * @author dupengcheng
  * @date 2024-04-15
  * @apiNote Gateway配置类
- *//*
+ */
 
 @Configuration
 public class GatewayConfig {
@@ -24,11 +22,10 @@ public class GatewayConfig {
                     // 请求路径匹配 "/guonei"
                     r -> r.path("/guonei")
                             // 目标URI为 "https://www.baidu.com"
-                            .uri("http://www.baidu.com"));
+                            .uri("http://news.baidu.com"));
 
             // 构建并返回整个路由定位器
             return routes.build();
         }
 
 }
-*/
