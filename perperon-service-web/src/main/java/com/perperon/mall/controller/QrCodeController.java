f27f8a02deff5c52cc3feee6b89c48cb36ab3b3b
Perperon <du1443528055@163.com>
2024-05-08 17:56:58 +0800

    二维码验证

---
+++ perperon-service-web/src/main/java/com/perperon/mall/controller/QrCodeController.java
@@ -0,0 +1,28 @@
+package com.perperon.mall.controller;
+
+import cn.hutool.http.server.HttpServerResponse;
+import org.iherus.codegen.Codectx;
+import org.iherus.codegen.qrcode.QrcodeGenerator;
+import org.iherus.codegen.qrcode.SimpleQrcodeGenerator;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * @author dupengcheng
+ * @date 2024-05-08
+ * @apiNote  二维码生成
+ */
+@RestController
+@RequestMapping("/qrCode")
+public class QrCodeController {
+
+    @GetMapping("/getQrCode")
+    public void getQrCode(HttpServerResponse response) throws IOException {
+        // 生成指定url对应的二维码到文件，宽和高都是300像素
+        QrCodeUtil.generate("https://hutool.cn/", 300, 300, FileUtil.file("d:/qrcode.jpg"));
+    }
+}
