21b6fc3a9301e10a56748ef1e596c64d04257787
Perperon <du1443528055@163.com>
2024-06-27 13:44:31 +0800

    接口修复

--- perperon-service-web/src/main/java/com/perperon/mall/controller/RolesController.java
+++ perperon-service-web/src/main/java/com/perperon/mall/controller/RolesController.java
@@ -1,15 +1,18 @@
 package com.perperon.mall.controller;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
 import com.perperon.mall.pojo.Roles;
 import com.perperon.mall.service.BaseService;
 import com.perperon.mall.service.RolesService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -33,11 +36,7 @@ public class RolesController extends BaseController<Roles>{
     @PostMapping("/grantRole")
     @ApiOperation(value = "用户加角色", notes = "用户加角色")
     public Object grantRole(@RequestBody Map<String, Object> params) {
-        System.out.println(params.get("rolesIds").toString());
-        JSONArray json = JSON.parseArray(params.get("rolesIds").toString());
-        System.out.println(json);
-
-        //rolesService.grantRole(params.get("accountId").toString(), rolesIds);
-        return null;
+        List<String> rolesIds = (ArrayList) params.get("rolesIds");
+        return rolesService.grantRole(params.get("accountId").toString(), rolesIds);
     }
 }
