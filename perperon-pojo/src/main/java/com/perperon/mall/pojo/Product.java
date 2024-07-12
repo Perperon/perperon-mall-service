6faea2ee9433e22378ed8513cf8c3c488044a8b2
Perperon <du1443528055@163.com>
2024-07-12 17:54:39 +0800

    增加商品类

---
+++ perperon-pojo/src/main/java/com/perperon/mall/pojo/Product.java
@@ -0,0 +1,53 @@
+package com.perperon.mall.pojo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 商品表;
+ * @author : http://www.chiner.pro
+ * @date : 2024-7-12
+ */
+
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+@ApiModel(value = "商品表",description = "")
+@Table(name="pp_product")
+public class Product implements Serializable,Cloneable{
+    /** 主键标识 */
+    @Id
+    @GeneratedValue
+    @ApiModelProperty(name = "主键标识",notes = "")
+    private String id ;
+    /** 名称 */
+    @ApiModelProperty(name = "名称",notes = "")
+    private String name ;
+    /** 商品类型 */
+    @ApiModelProperty(name = "商品类型",notes = "")
+    private String categoryId ;
+    /** 状态 */
+    @ApiModelProperty(name = "状态",notes = "")
+    private Boolean  status ;
+    /** 创建人 */
+    @ApiModelProperty(name = "创建人",notes = "")
+    private String userId ;
+    /** 创建时间 */
+    @ApiModelProperty(name = "创建时间",notes = "")
+    private Date created ;
+    /** 更新人 */
+    @ApiModelProperty(name = "更新人",notes = "")
+    private String updateBy ;
+    /** 更新时间 */
+    @ApiModelProperty(name = "更新时间",notes = "")
+    private Date update ;
+}
---
+++ perperon-pojo/src/main/java/com/perperon/mall/pojo/ProductCategory.java
@@ -0,0 +1,54 @@
+package com.perperon.mall.pojo;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @author dupengcheng
+ * @date 2024-07-12
+ * @apiNote
+ */
+
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+@ApiModel(value = "商品分类",description = "")
+@Table(name="pp_product_category")
+public class ProductCategory implements Serializable,Cloneable{
+
+    /** 主键标识 */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "select uuid()")
+    @ApiModelProperty(name = "主键标识",notes = "")
+    private String id ;
+    /** 商品分类名称 */
+    @ApiModelProperty(name = "商品分类名称",notes = "")
+    private String name ;
+    /** 状态 */
+    @ApiModelProperty(name = "状态",notes = "")
+    private Boolean status ;
+    /** 创建人 */
+    @ApiModelProperty(name = "创建人",notes = "")
+    @Column(name = "user_id")
+    private String userId ;
+    /** 创建时间 */
+    @ApiModelProperty(name = "创建时间",notes = "")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date created ;
+    /** 更新人 */
+    @ApiModelProperty(name = "更新人",notes = "")
+    @Column(name = "updated_by")
+    private String updatedBy ;
+    /** 更新时间 */
+    @ApiModelProperty(name = "更新时间",notes = "")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
+    private Date updated ;
+}
