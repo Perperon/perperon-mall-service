ef6f7be438eeef3a096180f0f913d6929a3c1ee5
Perperon <du1443528055@163.com>
2024-12-05 19:23:22 +0800

    MySimpleRunner提交

--- perperon-common/src/main/java/com/perperon/mall/common/config/MySimpleRunner.java
+++ perperon-common/src/main/java/com/perperon/mall/common/config/MySimpleRunner.java
@@ -1,12 +1,14 @@
 package com.perperon.mall.common.config;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
 /**
  * @author dupengcheng
  * @date 2024/11/10
  * @apiNote
  */
+@Component
 public class MySimpleRunner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
